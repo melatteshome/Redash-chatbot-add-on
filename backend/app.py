5d14211d671929593ca78a2fbea85946965f903b
eyerusalem <eyerusad12@gmail.com>
2024-01-06 23:41:14 +0300

    function name change

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def login():
+def data():
     if request.method == 'POST':
         return 
     else:
--- backend/util/utils.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-def get_csv_headers(csv_file_path):
-    with open(csv_file_path, 'r') as file:
-        reader = csv.reader(file)
-        headers = next(reader)
-    return headers
